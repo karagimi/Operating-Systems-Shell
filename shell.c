2d33e5ab5dfadfcdb5f2abf985b8f8dd74a1d584
karagimi-PC <karagimi@gmail.com>
2021-11-02 00:49:44 +0200

    update

--- shell.c
+++ shell.c
@@ -4,7 +4,7 @@
 
 /*pipes*/
 #define _GNU_SOURCE
-/*#include <fnct1.h>*/
+#include <fcntl.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -17,26 +17,52 @@
 #include <sys/types.h> /*fork*/
 
 #define TRUE 1
-#define SIZE 1000
+#define SIZE 100
+#define MAX_ARGS 100
 
 /*Prints the prompt to get a new command*/
 void type_prompt();
 
-/*Reads the command*/
-void read_command(char*);
-
 int main() {
-    char *input;
-   /* while(TRUE) {*/
+    char input[SIZE];
+    int status;
+    char* argv[MAX_ARGS];
+    while(TRUE) {
         type_prompt();
-        read_command(input);
+        argv[0]="";
+        argv[1]="";
 
-        if(fork()!=0) {
-            /*Parent code*/
-            /* waitpid(-1,&status,0);*/
+        int ch,count=0;
+        while((ch=getchar())!='\n') {
+            if(count==SIZE) {
+                break;
+            }
+            input[count]=ch;
+            count++;
+        }
+        if(strcmp(input,"ls")==0) {
+            argv[0]="/bin/ls";
+            argv[1]=NULL;
+        }
+
+        if(strcmp(input,"exit")==0) {
+            exit(0);
+        }
+
+        pid_t pid=fork();
+
+        if(pid==-1) {
+            perror("Fork error");
+            exit(EXIT_FAILURE);
+        }else if(pid==0) {
+             /*Child code*/
+            if (execv(argv[0], argv) < 0) {
+                printf("\nCould not execute command..");
+            }
+            exit(0);
         }else {
-            /*Child code*/
-            /*execve(command,parameters,0);*/
+            /*Parent code*/
+            wait(&status);
         }
     }
 }
@@ -47,6 +73,7 @@ void type_prompt() {
     char *user;
 
     user=getlogin();
+
     if(!user) {
         perror("getlogin() error");
     }
@@ -56,18 +83,6 @@ void type_prompt() {
     printf("@");
     printf("cs345sh");
     printf("%s",dir);
-    printf("$");
-    printf("\n");
+    printf("$ ");
 }
 
-void read_command(char *input) {
-    int ch,count=0;
-    while((ch=getchar())!='\n') {
-        if(count==SIZE) {
-            break;
-        }
-        input[count]=ch;
-        count++;
-    }
-    printf("%s",input);
-}
