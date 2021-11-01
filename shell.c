e85758478e7c07584244ee69cfa19b47681ba77a
karagimi <karagimi@gmail.com>
2021-11-01 15:04:35 +0200

    Update

--- shell.c
+++ shell.c
@@ -4,7 +4,7 @@
 
 /*pipes*/
 #define _GNU_SOURCE
-#include <fnct1.h>
+/*#include <fnct1.h>*/
 
 #include <stdio.h>
 #include <string.h>
@@ -17,26 +17,57 @@
 #include <sys/types.h> /*fork*/
 
 #define TRUE 1
+#define SIZE 1000
 
 /*Prints the prompt to get a new command*/
 void type_prompt();
 
+/*Reads the command*/
+void read_command(char*);
+
 int main() {
-    while(TRUE) {
+    char *input;
+   /* while(TRUE) {*/
         type_prompt();
-        read_command(command,parameters);
+        read_command(input);
 
         if(fork()!=0) {
             /*Parent code*/
-            waitpid(-1,&status,0);
+            /* waitpid(-1,&status,0);*/
         }else {
             /*Child code*/
-            execve(command,parameters,0);
+            /*execve(command,parameters,0);*/
         }
     }
 }
 
 /*Prints the prompt to get a new command*/
 void type_prompt() {
+    char dir[SIZE];
+    char *user;
 
+    user=getlogin();
+    if(!user) {
+        perror("getlogin() error");
+    }
+    getcwd(dir,SIZE);
+
+    printf("%s",user);
+    printf("@");
+    printf("cs345sh");
+    printf("%s",dir);
+    printf("$");
+    printf("\n");
+}
+
+void read_command(char *input) {
+    int ch,count=0;
+    while((ch=getchar())!='\n') {
+        if(count==SIZE) {
+            break;
+        }
+        input[count]=ch;
+        count++;
+    }
+    printf("%s",input);
 }
