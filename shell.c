96ffdf700d9656765783984d497a432bc6fe78f5
karagimi <karagimi@gmail.com>
2021-11-04 19:09:29 +0200

    Huge update

--- shell.c
+++ shell.c
@@ -23,25 +23,27 @@
 /*Prints the prompt to get a new command*/
 void type_prompt();
 
+char **get_command(char *input);
+
+char **get_sequential_command(char *input);
+
+void remove_spaces(char *input);
+
 int main() {
     char input[SIZE];
-    char input2[SIZE];
-    int status;
-    char* argv[MAX_ARGS];
-    char *user;
+    char *argv[SIZE];
+    char **command;
     char *token;
-    char directory[SIZE];
+    char *user;
     int index;
-    int semicolonCounter=0;
+    int count=0;
+    int command_count=0;
+    int command_index=0;
     while(TRUE) {
 
         type_prompt();
         int i=0;
         int ch,count=0;
-
-        memset(input,0,strlen(input));
-        argv[0]=NULL;
-        argv[1]=NULL;
 
         while((ch=getchar())!='\n') {
             if(count==SIZE) {
@@ -51,9 +53,20 @@ int main() {
             count++;
         }
         input[count]='\0';
-        strcpy(input2,input);
+        if(strchr(input,';')==NULL) { 
+            command=get_command(input);
+            if(strcmp(command[0],"cd") == 0 ){
+                if(command[1]==NULL) {
+                    chdir("/");
+                }else{
+                    chdir(command[1]);
+                }
+            }
 
-        if(strchr(input,';')==NULL) {
+            if(strcmp(command[0],"exit")==0) {
+                exit(0);
+            }
+
             pid_t pid=fork();
 
             if(pid==-1) {
@@ -61,120 +74,52 @@ int main() {
                 exit(EXIT_FAILURE);
             }else if(pid==0) {
                 /*Child code*/
-                if(strchr(input,'>')||(strchr(input,'<'))!=NULL) {
-             /*   int file=open("test.txt",O_WRONLY | O_CREAT,0777);
-                if(file == -1) {
-                    return 0;
-                }*/
-            }
-            token=strtok(input," "); 
-
-            if(strcmp(token,"ls")==0) {     /*ls*/
-                if(strlen(input2)==2) {
-                    argv[0]="/bin/ls";
-                    argv[1]=NULL;
-                }else if(strlen(input2)>2){
-                    argv[0]="/bin/ls";
-                    token=strtok(NULL," ");
-                    argv[1]=token;
-                    argv[2]=NULL;
-                }
-            }
-            
-            if(strcmp(input,"exit")==0) {   /*exit*/
-                exit(0);
-            }
-
-            if(strcmp(token,"cd")==0) {     /*cd*/
-                if(strlen(input2)==2) {
-                    /*chdir("/");*/
-                }else if(strlen(input2)>2) {
-                    token=strtok(NULL," ");
-                    strcpy(directory,token);
-                    chdir(directory);
-                }
-            }
-
-            if(strcmp(input,"pwd")==0) {    /*pwd*/
-                argv[0]="/bin/pwd";
-                argv[1]=NULL;
-            }
-
-            if(strcmp(input,"whoami")==0) { /*whoami*/
-                argv[0]="/usr/bin/whoami";
-                argv[1]=NULL;
-            }
-
-            if(strcmp(input,"mkdir")==0) {
-                token=strtok(NULL," ");
-                argv[0]="/bin/mkdir";
-                argv[1]=token;
-                argv[2]=NULL;
-            }
-
-            if(strcmp(input,"rmdir")==0) {
-                token=strtok(NULL," ");
-                argv[0]="/bin/rmdir";
-                argv[1]=token;
-                argv[2]=NULL;
-            }
-
-                execv(argv[0], argv);
-            /* printf("\nCould not execute command..\n");*/
+                execvp(command[0], command);
+                printf("\nCould not execute command..\n");
                 exit(0);
             }else {
                 /*Parent code*/
                 wait(NULL);
             }
-
-        }else{
-            for(index=0;index<strlen(input);index++) {
-                if(input[index]==';') {
-                    semicolonCounter++;
-                }
-            }
-            token=strtok(input,";");
+        }else {
+            
             index=0;
-            for(index=0;index<semicolonCounter+1;index++) {
-                pid_t pid2=fork();
-                if((strcmp(token,"ls ")==0)||(strcmp(token," ls")==0)||(strcmp(token," ls ")==0)) {
-                    argv[0]="/bin/ls";
-                    argv[1]=NULL;
-                }
+            command_count=0;
+            command_index=0;
+            command=get_sequential_command(input);
+            while(command[command_index]!=NULL) {
+                command_count++;
+                command_index++;
+            }
+            while(index<command_count) {
+                count=0;
+                pid_t pid=fork();
 
-                if((strcmp(token,"pwd ")==0)||(strcmp(token," pwd")==0)||(strcmp(token," pwd ")==0)) {
-                    argv[0]="/bin/pwd";
-                    argv[1]=NULL;
-                }
-
-                if((strcmp(token,"whoami ")==0)||(strcmp(token," whoami")==0)||(strcmp(token," whoami ")==0)) { /*whoami*/
-                    argv[0]="/usr/bin/whoami";
-                    argv[1]=NULL;
-                }
-
-                if((strcmp(token,"exit ")==0)||(strcmp(token," exit")==0)||(strcmp(token," exit ")==0)) {
-                    exit(0);
-                }
-
-                if(pid2==-1) {
+                if(pid==-1) {
                     perror("Fork error");
                     exit(EXIT_FAILURE);
-                }else if(pid2==0) {
-                    /*printf("token1 = %s\n",token);*/
-                    execv(argv[0], argv);
-                    /*printf("\nCould not execute command..\n");*/
+                }else if(pid==0) {
+                    /*Child code*/
+                    token=strtok(command[index]," ");
+                    while(token!=NULL) {
+                        argv[count]=token;
+                        /*printf("argv[%d] = %s\n",count,argv[count]);*/
+                        token=strtok(NULL," ");
+                        count++;
+                    }
+                    argv[count]=NULL;
+                    /*printf("command[index] = %s\n",command[index]);*/
+                    
+                    execvp(command[index],argv);
+                   /* printf("\nCould not execute command..\n");*/
                     exit(0);
-                }else {
+                }else{
                     /*Parent code*/
                     wait(NULL);
-                    /*printf("returned to parent\n");*/
                 }
-                token=strtok(NULL,";");
-                if(token==NULL) {
-                    break;
-                }
-                /*  printf("token2 = %s\n",token);*/
+                index++;
             }
+            /*free(command);*/
         }
     }
 }
@@ -196,4 +141,46 @@ void type_prompt() {
     printf("cs345sh");
     printf("%s",dir);
     printf("$ ");
+}
+
+char **get_command(char *input) {
+    char **command=malloc(100*sizeof(char *));
+    char *parsed;
+    int index=0;
+    parsed=strtok(input," ");
+    while(parsed!=NULL) {
+        command[index]=parsed;
+        index++;
+        parsed=strtok(NULL," ");
+    }
+    command[index]=NULL;
+    return command;
+}
+
+char **get_sequential_command(char *input) {
+    char **command=malloc(100*sizeof(char *));
+    char *parsed;
+    int index=0;
+    parsed=strtok(input,";");
+    while(parsed!=NULL) {
+        command[index]=parsed;
+        index++;
+       /* printf("parsed = %s\n",parsed);*/
+        parsed=strtok(NULL,";");
+        if((parsed!=NULL)) {
+            remove_spaces(parsed);
+        }
+        printf("parsed_After_remove = %s\n",parsed);
+    }
+    command[index]=NULL;
+    return command;
+}
+
+void remove_spaces(char *s) {
+    char*d =s;
+    do {
+        while(*d == ' ') {
+            ++d;
+        }
+    }while(*s++ = *d++);
 }
