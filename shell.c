b0d0ee571f5d5de1ad504cb73c78b50841b7e973
karagimi-PC <karagimi@gmail.com>
2021-11-06 19:24:43 +0200

    Update single pipe

--- shell.c
+++ shell.c
@@ -29,18 +29,31 @@ char **get_sequential_command(char *input);
 
 void remove_spaces(char *input);
 
-char **get_redirection_command(char *input);
+char **get_redirection_command_out(char *input);
+
+char **get_redirection_command_out_double(char *input);
+
+char **get_redirection_command_in(char *input);
+
+char **get_pipe1_command(char *input);
+
+char **get_pipe2_command(char *input);
 
 int main() {
     char input[SIZE];
+    char input2[SIZE];
     char *argv[SIZE];
     char **command;
+    char **command2;
     char *token;
     char *user;
     int index;
     int count=0;
     int command_count=0;
     int command_index=0;
+    int redirection_count;
+    int pipe_count;
+    int i=0;
     while(TRUE) {
 
         type_prompt();
@@ -55,62 +68,199 @@ int main() {
             count++;
         }
         input[count]='\0';
+
+        strcpy(input2,input);
         if(strchr(input,';')==NULL) { 
-            if(strchr(input,'>')!=NULL || strchr(input,'<')!=NULL) {
-                command=get_redirection_command(input);
-                pid_t pidred=fork();
-
-                if(pidred==-1) {
-                    perror("Fork error");
-                    exit(EXIT_FAILURE);
-                }else if(pidred==0) {
-                    /*Child code*/
-
-                    int file=open(command[1],O_WRONLY | O_CREAT | O_TRUNC,0777);
-                    if(file==-1) {
-                        return 0;
+            if(strchr(input,'>')!=NULL || strchr(input,'<')!=NULL) {    /*Redirections*/
+                if(strchr(input,'>')!=NULL && strchr(input,'<')==NULL) {    /*>*/
+                    redirection_count=0;
+                    for(i=0;i<strlen(input);i++) {
+                        if(input[i]=='>') {
+                            redirection_count++;
+                        }
                     }
 
-                    int file2=dup2(file,STDOUT_FILENO);
-                    close(file);
+                    if(redirection_count==1) {  /*>*/
+                        command=get_redirection_command_out(input);
+                        pid_t pidred=fork();
 
-                    command[1]=NULL;
+                        if(pidred==-1) {
+                            perror("Fork error");
+                            exit(EXIT_FAILURE);
+                        }else if(pidred==0) {
+                            /*Child code*/
+                            int command_count2=0;
+                            int command_index2=0;
+                            while(command[command_index2]!=NULL) {
+                                command_count2++;
+                                command_index2++;
+                            }
 
-                    execvp(command[0], command);
-                    printf("\nCould not execute command..\n");
-                    exit(0);
-                }else {
-                    /*Parent code*/
-                    wait(NULL);
+                            int file=open(command[command_index2-1],O_WRONLY | O_CREAT | O_TRUNC,0777);
+                            if(file==-1) {
+                                return 0;
+                            }
+
+                            command[command_index2-1]=NULL;
+
+                            int file2=dup2(file,STDOUT_FILENO);
+                            close(file);
+
+                            execvp(command[0], command);
+                           /* printf("\nCould not execute command..\n");*/
+                            exit(0);
+                        }else{
+                            /*Parent code*/
+                            wait(NULL);
+                        }
+                    }else if(redirection_count==2) {        /*>>*/
+                        command=get_redirection_command_out_double(input);
+                        pid_t pidapp=fork();
+
+                        if(pidapp==-1) {
+                            perror("Fork error");
+                            exit(EXIT_FAILURE);
+                        }else if(pidapp==0) {
+
+                            int command_count3=0;
+                            int command_index3=0;
+                            while(command[command_index3]!=NULL) {
+                                command_count3++;
+                                command_index3++;
+                            }
+                            
+
+                            int file=open(command[command_index3-1],O_WRONLY | O_CREAT | O_APPEND,0777);
+                            if(file==-1) {
+                                return 0;
+                            }
+
+                            command[command_index3-1]=NULL;
+
+                            int file2=dup2(file,STDOUT_FILENO);
+                            close(file);
+
+                            execvp(command[0], command);
+                            printf("\nCould not execute command..\n");
+                            exit(0);
+                        }else{
+                            wait(NULL);
+                        }
+                    }
+                }else if(strchr(input,'<')!=NULL && strchr(input,'>')==NULL) {  /*<*/
+                    command=get_redirection_command_in(input);
+                    pid_t pidred2=fork();
+
+                    if(pidred2==-1) {
+                        perror("Fork error");
+                        exit(EXIT_FAILURE);
+                    }else if(pidred2==0) {
+                        /*Child code*/
+
+                        int file=open(command[1],O_RDONLY);
+                        if(file==-1) {
+                            printf("The file does not exist\n");
+                            return 0;
+                        }
+
+                        int file2=dup2(file,0);
+                        close(file);
+
+                        command[1]=NULL;
+
+                        execvp(command[0], command);
+                        printf("\nCould not execute command..\n");
+                        exit(0);
+                    }else{
+                        /*Parent code*/
+                        wait(NULL);
+                    }
                 }
 
             }else{
-                command=get_command(input);
-                if(strcmp(command[0],"cd") == 0 ){
-                    if(command[1]==NULL) {
-                        chdir("/");
-                    }else{
-                        chdir(command[1]);
+                if(strchr(input,'|')!=NULL) {   /* PIPES (=1) */
+                    pipe_count=0;
+
+                    for(i=0;i<strlen(input);i++) {
+                        if(input[i]=='|') {
+                            pipe_count++;
+                        }
                     }
-                }
 
-                if(strcmp(command[0],"exit")==0) {
-                    exit(0);
-                }
+                    if(pipe_count==1) {
+                        int pipefd[2];
+                        if(pipe(pipefd)==-1) {
+                            return -1;
+                        }
 
-                pid_t pid=fork();
+                        command=get_pipe1_command(input);
 
-                if(pid==-1) {
-                    perror("Fork error");
-                    exit(EXIT_FAILURE);
-                }else if(pid==0) {
-                    /*Child code*/
-                    execvp(command[0], command);
-                    printf("\nCould not execute command..\n");
-                    exit(0);
+                        int pidpipe = fork();
+
+                        if(pidpipe<0) {
+                            printf("Fork Error\n");
+                            return 0;
+                        }else if(pidpipe==0) {
+                            //Child Process 1
+
+                            dup2(pipefd[1],STDOUT_FILENO);
+                            close(pipefd[0]);
+                            close(pipefd[1]);
+                            execvp(command[0],command);
+                        }
+
+                        command2=get_pipe2_command(input2);
+
+                        int pidpipe2 = fork();
+                        if(pidpipe2 < 0) {
+                            return 3;
+                        }else if(pidpipe2==0) {
+                            //Child Process 2
+                            dup2(pipefd[0],STDIN_FILENO);
+                            close(pipefd[0]);
+                            close(pipefd[1]);
+                            execvp(command2[0],command2);
+                        }
+
+                        close(pipefd[0]);
+                        close(pipefd[1]);
+
+                        waitpid(pidpipe,NULL,0);
+                        waitpid(pidpipe2,NULL,0);
+                    }else{
+
+                        printf("MULTIPLE PIPES LMAO");
+  
+                    }
+
                 }else {
-                    /*Parent code*/
-                    wait(NULL);
+                    command=get_command(input);
+                    if(strcmp(command[0],"cd") == 0 ){
+                        if(command[1]==NULL) {
+                            chdir("/");
+                        }else{
+                            chdir(command[1]);
+                        }
+                    }
+
+                    if(strcmp(command[0],"exit")==0) {
+                        exit(0);
+                    }
+
+                    pid_t pid=fork();
+
+                    if(pid==-1) {
+                        perror("Fork error");
+                        exit(EXIT_FAILURE);
+                    }else if(pid==0) {
+                        /*Child code*/
+                        execvp(command[0], command);
+                        printf("\nCould not execute command..\n");
+                        exit(0);
+                    }else {
+                        /*Parent code*/
+                        wait(NULL);
+                    }
                 }
             }
         }else{
@@ -135,16 +285,14 @@ int main() {
                     token=strtok(command[index]," ");
                     while(token!=NULL) {
                         argv[count]=token;
-                        /*printf("argv[%d] = %s\n",count,argv[count]);*/
                         token=strtok(NULL," ");
                         count++;
                     }
                     argv[count]=NULL;
-                    /*printf("command[index] = %s\n",command[index]);*/
-                        
+            
                     execvp(command[index],argv);
-                    /* printf("\nCould not execute command..\n");*/
-                        exit(0);
+                    printf("\nCould not execute command..\n");
+                    exit(0);
                 }else{
                     /*Parent code*/
                     wait(NULL);
@@ -198,12 +346,11 @@ char **get_sequential_command(char *input) {
     while(parsed!=NULL) {
         command[index]=parsed;
         index++;
-       /* printf("parsed = %s\n",parsed);*/
+        printf("parsed = %s\n",parsed);
         parsed=strtok(NULL,";");
-        if((parsed!=NULL)) {
+      /*  if(parsed!=NULL) {
             remove_spaces(parsed);
-        }
-        printf("parsed_After_remove = %s\n",parsed);
+        }*/
     }
     command[index]=NULL;
     return command;
@@ -211,27 +358,113 @@ char **get_sequential_command(char *input) {
 
 void remove_spaces(char *s) {
     char*d =s;
-    do {
-        while(*d == ' ') {
-            ++d;
+    int i=0;
+    int count=0;
+    for(i=0;i<strlen(d);i++) {
+        if(d[i]==' ') {
+            count++;
         }
-    }while(*s++ = *d++);
+    }
+    if(count==1) {
+        do {
+            while(*d == ' ') {
+                ++d;
+            }
+        }while(*s++ = *d++);
+    }
 }
 
 
-char **get_redirection_command(char *input) {
+char **get_redirection_command_out(char *input) {
     char **command=malloc(100*sizeof(char *));
     char *parsed;
     int index=0;
-    parsed=strtok(input,">");
+    parsed=strtok(input," ");
     while(parsed!=NULL) {
+        if(strchr(parsed,'>')!=NULL) {
+            parsed=strtok(NULL," ");
+            break;
+        }
         command[index]=parsed;
         index++;
         if((parsed!=NULL)) {
             remove_spaces(parsed);
         }
-        printf("parsed = %s\n",parsed);
-        parsed=strtok(NULL,">");
+        parsed=strtok(NULL," ");
+    }
+    command[index]=parsed;
+    command[index+1]=NULL;
+    return command;
+}
+
+char **get_redirection_command_out_double(char *input) {
+    char **command=malloc(100*sizeof(char *));
+    char *parsed;
+    int index=0;
+    parsed=strtok(input," ");
+    while(parsed!=NULL) {
+        if(strcmp(parsed,">>")==0) {
+            parsed=strtok(NULL," ");
+            break;
+        }
+        command[index]=parsed;
+        index++;
+        if((parsed!=NULL)) {
+            remove_spaces(parsed);
+        }
+        parsed=strtok(NULL," ");
+    }
+    command[index]=parsed;
+    command[index+1]=NULL;
+    return command;
+}
+
+char **get_redirection_command_in(char *input) {
+    char **command=malloc(100*sizeof(char *));
+    char *parsed;
+    int index=0;
+    parsed=strtok(input," ");
+    while(parsed!=NULL) {
+        if(strchr(parsed,'<')!=NULL) {
+            parsed=strtok(NULL," ");
+            break;
+        }
+        command[index]=parsed;
+        index++;
+        if((parsed!=NULL)) {
+            remove_spaces(parsed);
+        }
+        parsed=strtok(NULL," ");
+    }
+    command[index]=parsed;
+    command[index+1]=NULL;
+    return command;
+}
+
+char **get_pipe1_command(char *input) {
+    char **command=malloc(100*sizeof(char *));
+    char *parsed;
+    int index=0;
+    parsed=strtok(input," ");
+    while(strchr(parsed,'|')==NULL) {
+        command[index]=parsed;
+        index++;
+        parsed=strtok(NULL," ");
+    }
+
+    command[index]=NULL;
+    return command;
+}
+
+char **get_pipe2_command(char *input) {
+    char **command=malloc(100*sizeof(char *));
+    char *parsed;
+    int index=0;
+    parsed=strtok(input,"|");
+    while(parsed!=NULL) {
+        parsed=strtok(NULL," ");
+        command[index]=parsed;
+        index++;
     }
     command[index]=NULL;
     return command;
